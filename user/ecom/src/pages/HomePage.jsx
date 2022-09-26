981965a837f3258a8767658280582b0846cb9ffd
Fatih <tuncer.fm@gmail.com>
2022-09-26 11:37:20 +0300

    slider and category done

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
 import React, { Component, Fragment } from 'react'
 import FeaturedProducts from '../components/home/FeaturedProducts';
+import HomeTop from '../components/home/HomeTop'
+import Collection from '../components/home/Collection';
 import Categories from '../components/home/Categories';
+import NewArrival from '../components/home/NewArrival';
+
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <HomeTop />
         <FeaturedProducts/>
+        <NewArrival />
+        <Collection />
         <Categories />
       </Fragment>
     )
